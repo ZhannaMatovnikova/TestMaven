f46c6325867b348f3d24e5e048697b7d0154e8c6
Жанна <jane95@outlook.com>
2023-12-08 11:40:27 +0300

    работа с таблицами

--- src/main/java/Tables.java
+++ src/main/java/Tables.java
@@ -10,11 +10,20 @@ public class Tables {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Жанна\\IdeaProjects\\untitled2\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //implicitly - драйвер выжидает 10 сек перед поиском элемента на странице
         driver.manage().window().maximize();
 
         driver.get("https://www.w3schools.com/html/html_tables.asp");
         WebElement tableElement = driver.findElement(By.xpath("//table[@id=\"customers\"]"));
+        TablesMethods table = new TablesMethods(tableElement, driver);
+
+
+        System.out.println("Raws number is: " + table.getRows().size());
+        System.out.println(table.getValueFromCell(2,3));
+        System.out.println(table.getValueFromCell(4,1));
+        System.out.println(table.getValueFromCell(4,"Company"));
+        System.out.println(table.getValueFromCell(1,"Country"));
+        System.out.println(table.getValueFromCell(2,"Contact"));
 
 
         driver.quit();
--- src/main/java/TablesMethods.java
+++ src/main/java/TablesMethods.java
@@ -3,7 +3,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TablesMethods {
 
@@ -37,11 +39,38 @@ public class TablesMethods {
         return rowsWithColumns;
     }
 
-    //метод, который позволит использовать метод getRowsWithColumns
+
+    //получение значения по номеру строки и заголовку
+    public List<Map<String, WebElement>> getRowsWithColumnsByHeadings(){ //лист - список всех строк, мап -каждая строка
+        // string заголовок, webelement - конкретная ячейка
+        List<List<WebElement>> rowsWithColumns = getRowsWithColumns();
+        List<Map<String, WebElement>> rowsWithColumnsByHeadings = new ArrayList<Map<String, WebElement>>(); //те строки, разбитые по заголовкам, которые метод будет возвращать
+        Map<String, WebElement> rowByHeadings;    //одна строка, разбитая на столбцы по заголовкам,  такие строки будут содержаться в листе
+        List<WebElement> headingColumns = getHeadings();
+
+        for (List<WebElement> row: rowsWithColumns) {
+            rowByHeadings = new HashMap<String, WebElement>();
+            for (int i = 0; i<headingColumns.size(); i++){
+                String heading = headingColumns.get(i).getText();// перебираем каждую строку, чтобы получить текст заголовка и привязать заголовок к колонке
+                WebElement cell = row.get(i); // получаем ячейку из строки под тем же номером, что и заголовок
+                rowByHeadings.put(heading,cell);
+            }
+            rowsWithColumnsByHeadings.add(rowByHeadings);
+        }
+        return rowsWithColumnsByHeadings;
+    }
+
+    //метод, который позволит использовать метод getRowsWithColumns номер строки и номер столбца
     public String getValueFromCell(int rowNumber, int columnNumber){
         List<List<WebElement>> rowsWithColumns = getRowsWithColumns(); // получение списка столбцов
         WebElement cell = rowsWithColumns.get(rowNumber-1).get(columnNumber-1);
         // создаем ячейку, получаем строку через номер строки, получаем столбец по номеру столбца
         return cell.getText();
     }
+    //метод, который позволит использовать метод getRowsWithColumns номер строки и имя заголовка
+    public String getValueFromCell(int rowNumber, String columnName){
+        List<Map<String,WebElement>> rowsWithColumnsByHeadings = getRowsWithColumnsByHeadings(); // получение списка столбцов
+        return rowsWithColumnsByHeadings.get(rowNumber-1).get(columnName).getText();
+    }
+
 }
