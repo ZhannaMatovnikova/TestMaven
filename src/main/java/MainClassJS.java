4f16190ef2979d7974de4a11fecc8d86cb39b71e
Жанна <jane95@outlook.com>
2023-12-11 21:19:25 +0300

    js: alert, scrloll

--- src/main/java/MainClassJS.java
+++ src/main/java/MainClassJS.java
@@ -12,20 +12,60 @@ public class MainClassJS {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.get("https://en.wikipedia.org/");
 
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
+
+//        driver.get("https://qa-ep-bva-practice-assignment.vercel.app/");
+
+//        driver.findElement(By.xpath("//button[text()=\'Validate\']")).click();//появляется алерт
+//        try {                                                           //ожидание 3 сек
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        driver.switchTo().alert().accept();//нажимаем ок на алерт, принимаем его
+
+
+
+
+
+//        driver.get("http://google.com");
+//        JavascriptExecutor js1 = (JavascriptExecutor)driver;
+//        js1.executeScript("confirm('Are you sure?');");//создаем алерт на сайте
+//        try {                                                           //ожидание 3 сек
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+////        driver.switchTo().alert().accept();//принимаем алерт
+//        driver.switchTo().alert().dismiss();//отклоняем алерт
+
+
+
+
+
+//        driver.get("https://en.wikipedia.org/");
+
+//        JavascriptExecutor jse = (JavascriptExecutor)driver;
 //        jse.executeScript("alert('Hello world!');"); // получаем в браузере окно с просьбой подтвердить4
-        jse.executeScript("window.scrollBy(0, 1000)",""); //прокрутка страницы вниз
-        jse.executeScript("window.scrollBy(0, -1000)",""); //прокрутка страницы вверх
+//        jse.executeScript("window.scrollBy(0, 1000)",""); //прокрутка страницы вниз
+//        jse.executeScript("window.scrollBy(0, -1000)",""); //прокрутка страницы вверх
+
+        driver.get("https://www.oracle.com/java/technologies/java-archive-misc-downloads.html#jdk8demos");
+        driver.findElement(By.cssSelector("body > div.f11w1 > section:nth-child(7) > div > p > a")).click();
+        //при нажатии на ссылку открывается новое окно браузера
+        String mainWindow = driver.getWindowHandle(); //задаем главное окно
+
+        for (String windowHandle:driver.getWindowHandles()){ //переключаемся на новое окно
+            driver.switchTo().window(windowHandle);
+        }
+
+        driver.findElement(By.xpath("//button[text()='Products']")).click();
+        driver.switchTo().window(mainWindow);
+
+        driver.findElement(By.cssSelector("#industries1")).click();
 
 
 
-
-//        driver.get("https://www.ebay.com/");
-//
-//        WebElement link = driver.findElement(By.xpath("//li[@class='hl-cat-nav__js-tab']//a[text()='Электроника']"));
-//        WebElement element = driver.findElement(By.xpath("//li[@class='hl-cat-nav__js-tab']//a[text()='Электроника']"));
 
 //      driver.quit();
 
