99e22f5371b75af8008c1540b93132a83ddeb655
Жанна <jane95@outlook.com>
2023-12-14 10:10:49 +0300

    ms edge разобраться

--- src/main/java/EdgeTest.java
+++ src/main/java/EdgeTest.java
@@ -1,3 +1,10 @@
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
 //
-
-//для java 1.8
+//public class EdgeTest {
+//    public static void main(String[] args) {
+//        System.setProperty("webdriver.edge.driver", "C:\\Users\\Жанна\\IdeaProjects\\untitled2\\drivers\\msedgedriver.exe");
+//        WebDriver driver = new EdgeDriver();
+//    }
+//}
+// для java 1.8 запуск браузера ms Edge
